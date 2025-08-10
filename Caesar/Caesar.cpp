ad660e845b6204fe2e5ad6af6c8e3beb767a87ae
newenpoi <lammenschristopher119@hotmail.com>
2025-08-10 12:03:55 +0200

    fully functional caesar

--- Caesar/Caesar.cpp
+++ Caesar/Caesar.cpp
@@ -14,14 +14,14 @@ using namespace std;
 * Tout d'abord tester uniquement avec des minuscules (on procède étape par étape).
 * Gérer maintenant les majuscules et les minuscules (décalage identique mais conserver la casse).
 * Les caractères spéciaux ne changent pas.
-* Peut être que str.at() serait utile.
 */
 
 int main()
 {
 	string input, output;
 	int offset = 3;
-	string characters = "abcdefghijklmnopqrstuvwxyz";
+	string lowercase = "abcdefghijklmnopqrstuvwxyz";
+	string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	// Attente de l'entrée utilisateur.
 	cout << u8"Entrez la chaîne de caractères à encoder." << endl;
@@ -30,14 +30,50 @@ int main()
 	// Encodage grâce à une boucle for où i est le parcours de l'input par rapport à sa taille.
 	for (int i = 0, j = 0; i < input.size(); i++)
 	{
-		// Récupère la position du caractère.
-		size_t pos = characters.find(input[i]);
+		// Uppercase.
+		if (input[i] >= 65 && input[i] <= 90)
+		{
+			// Récupère la position du caractère.
+			size_t pos = uppercase.find(input[i]);
 
-		// Gère les limites de l'offset acceptables grâce à modulo.
-		size_t new_pos = (pos + offset) % characters.size();
+			// Gère les limites de l'offset acceptables grâce à modulo.
+			size_t new_pos = (pos + offset) % uppercase.size();
 
-		// Ajoute le caractère avec un décalage (encodage).
-		output.push_back(characters[new_pos]);
+			// Ajoute le caractère avec un décalage (encodage).
+			output.push_back(uppercase[new_pos]);
+		}
+
+		// Lowercase.
+		else if (input[i] >= 97 && input[i] <= 122)
+		{
+			// Récupère la position du caractère.
+			size_t pos = lowercase.find(input[i]);
+			
+			// Gère les limites de l'offset acceptables grâce à modulo.
+			size_t new_pos = (pos + offset) % lowercase.size();
+			
+			// Ajoute le caractère avec un décalage (encodage).
+			output.push_back(lowercase[new_pos]);
+		}
+
+		// Numerics.
+		else if (input[i] >= 48 && input[i] <= 57)
+		{
+			// Récupère la position du caractère (converti le caractère en entier).
+			size_t pos = input[i] - '0';
+			
+			// Gère les limites de l'offset acceptables grâce à modulo.
+			size_t new_pos = (pos + offset) % 10;
+			
+			// Ajoute le caractère avec un décalage (encodage).
+			output.push_back(new_pos + '0');
+		}
+
+		else
+		{
+			// Pour tout autre caractère, on le laisse tel quel.
+			output.push_back(input[i]);
+		}
 	}
 
 	cout << "Résultat de l'encodage :" << endl;
